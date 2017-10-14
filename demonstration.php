a9f58df9025792d99abcfb01512bad019345eb7c
Benas Rimsa <benas.rimsa@gmail.com>
2017-10-14 05:13:51 +0300

    update demonstration.php file

--- demonstration.php
+++ demonstration.php
@@ -34,41 +34,41 @@ $obj->runTest('in object context');
 //__callStatic() is triggered when invoking inaccessible methods in a static context.
 Method::runTest('in static context');
 
-$obj = new last_method();
-$obj->name = "Benas";
-$obj->lastName = "Rimsa";
+$obj1 = new last_method();
+$obj1->name = "Benas";
+$obj1->lastName = "Rimsa";
 //serialize() checks if your class has a function with the magic name __sleep().
 // If so, that function is executed prior to any serialization.
 // It can clean up the object and is supposed to return an array with the names of all
 // variables of that object that should be serialized.
-$obj = serialize($obj);
-echo $obj . PHP_EOL . "</br>";
+$obj1 = serialize($obj1);
+echo $obj1 . PHP_EOL . "</br>";
 //Conversely, unserialize() checks for the presence of a function with
 // the magic name __wakeup(). If present, this function can reconstruct any
 // resources that the object may have.
-$obj = unserialize($obj);
-echo "Name: " . $obj->name . PHP_EOL . "</br>";
+$obj1 = unserialize($obj1);
+echo "Name: " . $obj1->name . PHP_EOL . "</br>";
 
 //The __toString() method allows a class to decide how it
 // will react when it is treated like a string.
-echo $obj . PHP_EOL;
+echo $obj1 . PHP_EOL;
 
 //The __invoke() method is called when a script tries to call an object as a function.
-$obj(" result String");
+$obj1(" result String");
 
 //The only parameter of this method is an array containing exported properties
 // in the form array('property' => value, ...).
-eval('$exportTest = ' . var_export($obj, true) . ';');
+eval('$exportTest = ' . var_export($obj1, true) . ';');
 echo "Name: " . $exportTest->name . " LastName: " . $exportTest->lastName . PHP_EOL . "</br>";
 
 //Once the cloning is complete, if a __clone() method is defined,
 // then the newly created object's __clone() method will be called,
 // to allow any necessary properties that need to be changed.
-echo "object name: " . $obj->name . PHP_EOL . "</br>";
-$obj2 = clone $obj;
+echo "object name: " . $obj1->name . PHP_EOL . "</br>";
+$obj2 = clone $obj1;
 echo "cloned object name: " . $obj2->name . PHP_EOL . "</br>";
 
 //This method is called by var_dump() when dumping an object to get the properties
 // that should be shown. If the method isn't defined on an object,
 // then all public, protected and private properties will be shown.
-var_dump($obj);
+var_dump($obj1);
